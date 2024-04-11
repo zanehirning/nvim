6df27245fdbb8adb291d5cdf7ae132b4e9c70b02
zanehirning <a02371568@usu.edu>
2024-04-10 22:16:27 -0600

    fix functionality

--- lua/zanehirning/lazyconfig/telescope.lua
+++ lua/zanehirning/lazyconfig/telescope.lua
@@ -6,6 +6,8 @@ return {
     },
     tag = "0.1.5",
     config = function ()
+        require('telescope').setup({})
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
