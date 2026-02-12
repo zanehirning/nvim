893e7abd347cfebf164166c4572d206d16960f9b
Zane <zanehirning41@gmail.com>
2026-02-11 20:13:36 -0700

    add lsp line hints and clean up unused things in lsp.lua

--- lua/zanehirning/lazyconfig/init.lua
+++ lua/zanehirning/lazyconfig/init.lua
@@ -3,10 +3,4 @@ return {
         "nvim-lua/plenary.nvim",
         name = "plenary"
     },
-
-    -- "github/copilot.vim"
-    -- {
-    --     "github/copilot.vim",
-    --     name = "copilot"
-    -- }
 }
--- lua/zanehirning/lazyconfig/lsp.lua
+++ lua/zanehirning/lazyconfig/lsp.lua
@@ -1,106 +1,82 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
-
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "luvit-meta/library",      words = { "vim%.uv" } },
+                        { path = "/usr/share/awesome/lib/", words = { "awesome" } },
+                    },
+                },
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        config = function()
+            -- Don't do LSP stuff if we're in Obsidian Edit mode
+            local capabilities = nil
+            if pcall(require, "cmp_nvim_lsp") then
+                capabilities = require("cmp_nvim_lsp").default_capabilities()
+            end
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
+            local servers = {
+                lua_ls = {
+                    cmd = { "lua-language-server" },
+                },
+                clangd = {},
             }
-        })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local servers_to_install = vim.tbl_filter(function(key)
+                local t = servers[key]
+                if type(t) == "table" then
+                    return not t.manual_install
+                else
+                    return t
+                end
+            end, vim.tbl_keys(servers))
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            }),
-        })
+            require("mason").setup()
+            local ensure_installed = {
+                "stylua",
+                "lua_ls",
+                "clangd"
+            }
 
-        vim.diagnostic.config({
-            update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
+            vim.list_extend(ensure_installed, servers_to_install)
+            require("mason-tool-installer").setup { ensure_installed = ensure_installed }
+
+            -- Set global capabilities for all LSP servers
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
+
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                update_in_insert = false
+            })
+
+            -- Configure and enable each LSP server
+            for name, config in pairs(servers) do
+                if config == true then
+                    config = {}
+                end
+
+                -- Only call vim.lsp.config if there are server-specific settings
+                if next(config) ~= nil then
+                    -- Remove manual_install flag as it's not an LSP config field
+                    local lsp_config = vim.tbl_deep_extend("force", {}, config)
+                    lsp_config.manual_install = nil
+                    vim.lsp.config(name, lsp_config)
+                end
+
+                vim.lsp.enable(name)
+            end
+        end,
+    },
 }
